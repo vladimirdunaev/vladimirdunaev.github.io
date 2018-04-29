0a7696d4e91869e16a31430098cede96973c700a
vladimirdunaev <dunaevvolodya@gmail.com>
2018-04-29 19:34:18 +0300

    29.04.18-19:30

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 /*JavaScript*/
+
+/*scrollup*/
+$(document).ready(function(){
+ 
+  $(window).scroll(function(){
+  if ($(this).scrollTop() > 300) {
+  $('.scrollup').fadeIn();
+  } else {
+  $('.scrollup').fadeOut();
+  }
+  });
+
+  $('.scrollup').click(function(){
+    $("html, body").animate({ scrollTop: 0 }, 1000);
+    return false;
+  });
+ 
+});
